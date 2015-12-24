278eeb62bf2190ecc858458d54877497efcc35b0
HackDeveloperUA <hack@mac-mini-hack.local>
2015-12-24 16:03:55 +0300

    Initial Commit

---
+++ SportNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SportNews
+//
+//  Created by Hack on 12/24/15.
+//  Copyright © 2015 TestJson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
